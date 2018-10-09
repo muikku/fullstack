e952ef115e5bb0af11b03144aed02a3d5970591c
Markus Muinonen <markus.muinonen@gmail.com>
2018-10-09 14:27:42 +0300

    test dummy ans total likes

--- osa4/blogilista/tests/dummytest.test.js
+++ osa4/blogilista/tests/dummytest.test.js
@@ -1,8 +1,13 @@
 const listHelper = require('../utils/list_helper')
-const blogs = require('../utils/testblog')
+const blogs = require('../utils/testblog').testBlogs
 
 
 test('dummy is called', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+test('total likes', () => {
+  const result = listHelper.totalLikes(blogs)
+  expect(result).toBe(36)
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (Blogs) => {
+  return Blogs.map(e => e.likes).reduce((acc, cur) => acc + cur)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
