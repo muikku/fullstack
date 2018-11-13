d09013b5e241834899547c8a9711f74e2957310d
Markus Muinonen <markus.muinonen@gmail.com>
2018-11-13 14:39:06 +0200

    right repo

--- osa6/routed-anecdotes/src/components/Footer.js
+++ osa6/routed-anecdotes/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
     <div className="ui black inverted attached bottom segment">
       Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
     
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+    See <a href='https://github.com/muikku/fullstack/tree/master/osa6/redux-anecdotes-v2'>https://github.com/muikku/fullstack/tree/master/osa6/redux-anecdotes-v2</a> for the source code. 
     
     </div>
     )
